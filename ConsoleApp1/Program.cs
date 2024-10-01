292fd35bbd65cafe5d9ad1ad22ac5991df0af016
irl <irl@DESKTOP-DIAN65C>
2024-10-01 21:25:39 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Melis Yıldız");
+Console.WriteLine("patika'yı çevremdeki insanlardan çok duydum,herkes memnuniyetini dile getirdiği için kodlama dünyasına patika bootcampi ile giriş yapmak istedim.");
